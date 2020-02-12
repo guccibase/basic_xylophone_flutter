82a91b6aa653b397d78e9077a1643dd65335767a
guccibase <tahirnasri96@gmail.com>
2020-02-12 12:07:51 -0500

    xylo

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  play({String file, Color color}) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            final player = AudioCache();
+            player.play(file);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            play(file: 'note1.wav', color: Colors.pinkAccent),
+            play(file: 'note2.wav', color: Colors.blue),
+            play(file: 'note3.wav', color: Colors.teal),
+            play(file: 'note4.wav', color: Colors.red),
+            play(file: 'note5.wav', color: Colors.yellow),
+            play(file: 'note6.wav', color: Colors.grey),
+            play(file: 'note7.wav', color: Colors.purple),
+          ],
+        )),
       ),
     );
   }
